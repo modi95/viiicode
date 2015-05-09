7b07d3ae0ba636c102cdf3d1241ee6a14950be0d
Abhishek Modi <abhishek.modi95@gmail.com>
2015-05-09 01:24:55 -0500

    Initial Commit

---
+++ Makefile
@@ -0,0 +1,14 @@
+# VIII CODE
+# Abhishek K Modi
+
+CC = clang
+CFLAGS = -g -W -Wall -Wextra
+EXEFLAGS = -o 
+
+OBJECTS = 
+
+c_viiicode: viiicode.c
+	
+
+clean:
+	rm -rf *.o c_viiicode
---
+++ viii_code.py
@@ -0,0 +1,19 @@
+# viii_code.py
+# Brainfuck interpeter
+#
+# Abhishek Modi
+# www.akmodi.com
+
+import sys
+import os
+
+def file_to_array(src_file_name):
+	src_file = open(src_file_name)
+	with src_file as 
+
+def main():
+	src = file_to_array(sys.argv[1])
+	print src
+	return
+
+main()
